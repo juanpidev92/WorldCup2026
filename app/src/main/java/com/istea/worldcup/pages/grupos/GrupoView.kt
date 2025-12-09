2aee5ac7a5bfdfcae07a845c6de874f25806b044
juanpidev92 <juan.mancebo@istea.com.ar>
2025-12-09 23:55:24 +0100

    Apunto al logo de la fifa

--- app/src/main/java/com/istea/worldcup/pages/grupos/GrupoView.kt
+++ app/src/main/java/com/istea/worldcup/pages/grupos/GrupoView.kt
@@ -105,7 +105,7 @@ fun GroupCardView(
 @Composable
 fun BackgroundImage() {
     Image(
-        painter = painterResource(R.drawable.logo_viejo),
+        painter = painterResource(R.drawable.logo),
         contentDescription = null,
         contentScale = ContentScale.Crop,
         modifier = Modifier
